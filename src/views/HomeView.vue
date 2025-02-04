43c22361a2cacaaf9881967b9d60b742212488a3
danny_chen <danny_chen@seberon.com>
2025-02-04 11:04:33 +0800

    feat: Add Card and Button components to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,18 @@
 <script setup></script>
 <template>
-  <div>Home</div>
+  <Card class="mt-4">
+    <template #title>Simple Card</template>
+    <template #content>
+      <p class="m-0">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed
+        consequuntur error repudiandae numquam deserunt quisquam repellat libero
+        asperiores earum nam nobis, culpa ratione quam perferendis esse,
+        cupiditate neque quas!
+      </p>
+    </template>
+  </Card>
+
+  <Button>
+    <h1 class="text-white">測試</h1>
+  </Button>
 </template>
